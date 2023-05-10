94c70300442cc4a36d001e36a6cb7eaa8ca74967
yuktnk <yuki0wl@gmail.com>
2023-05-11 08:27:36 +0900

    Hasuraのデータフェッチサンプル

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,37 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+import { GET_USERS } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+
+  if (error) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+      {console.log(data)}
+      {data?.users.map((user) => {
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export default FetchMain
