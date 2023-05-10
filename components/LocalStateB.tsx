725cefad8e72b8bf1f30023a8168391efdeeb052
yuktnk <yuki0wl@gmail.com>
2023-05-10 09:16:57 +0900

    create new components

---
+++ components/LocalStateB.tsx
@@ -0,0 +1,23 @@
+import { VFC } from 'react'
+import { todoVar } from '../cache'
+import { useReactiveVar } from '@apollo/client'
+import Link from 'next/link'
+
+export const LocalStateB: VFC = () => {
+  const todos = useReactiveVar(todoVar)
+
+  return (
+    <>
+      {todos?.map((task, index) => {
+        return (
+          <p className="mb-3" key={index}>
+            {task.title}
+          </p>
+        )
+      })}
+      <Link href="/local-state-a">
+        <a>Back</a>
+      </Link>
+    </>
+  )
+}
