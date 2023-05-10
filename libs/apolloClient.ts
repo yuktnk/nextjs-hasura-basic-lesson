be0e28b0ae22ffbe65f6bffd952bbc03f41ab929
yuktnk <yuki0wl@gmail.com>
2023-05-11 08:25:29 +0900

    Apolloクライアントの初期化/作成ロジッく

---
+++ libs/apolloClient.ts
@@ -0,0 +1,44 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import 'cross-fetch/polyfill'
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE'
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined // 初期値のセットなし
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: 'https://notable-lacewing-49.hasura.app/v1/graphql',
+    }),
+    cache: new InMemoryCache(),
+  })
+}
+
+// Apolloクライアントの初期化
+// - サーバー側: Apolloクライアントを初期化して返す
+// - クライアント側: 既に初期化されたApolloクライアントを使用する
+// Apolloクライアントがまだ作成されていない場合には、
+// 作成してグローバル変数apolloClientに代入することで、再利用性を高めている
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient()
+
+  // 環境がサーバー側（SSG or SSR）である場合、
+  // 毎回作成されたApolloクライアントを返す。
+  if (typeof window === 'undefined') {
+    return _apolloClient
+  }
+
+  // Apolloクライアントがまだ作成されていない場合、
+  // 作成した_apolloClientをapolloClient変数に代入する。
+  if (!apolloClient) {
+    apolloClient = _apolloClient
+  }
+
+  return _apolloClient
+}
