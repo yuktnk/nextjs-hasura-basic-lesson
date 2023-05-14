6179c47d7a4668faaf3e7178a5dddad3c69f16ac
yuktnk <yuki0wl@gmail.com>
2023-05-14 18:54:08 +0900

    Custom Hook + useCallback + memo

---
+++ components/Child.tsx
@@ -0,0 +1,22 @@
+import { FormEvent, memo, VFC } from 'react'
+
+interface Props {
+  printMsg: () => void
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>
+}
+
+// eslint-disable-next-line react/display-name
+export const Child: VFC<Props> = memo(({ printMsg, handleSubmit }) => {
+  return (
+    <>
+      {console.log('Childコンポーネントのレンダリング')}
+      <p>Child Component</p>
+      <button
+        className="my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+        onClick={printMsg}
+      >
+        click
+      </button>
+    </>
+  )
+})
