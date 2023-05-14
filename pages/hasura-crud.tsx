253a67e482a5ba42eada7fb5f3446a000b128574
yuktnk <yuki0wl@gmail.com>
2023-05-14 16:35:38 +0900

    CRUD実装

--- pages/hasura-crud.tsx
+++ pages/hasura-crud.tsx
@@ -13,8 +13,11 @@ import {
   UpdateUserMutation,
 } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+import { UserItem } from '../components/UserItem'
 
 const HasuraCRUD: VFC = () => {
+  const [editedUser, setEditedUser] = useState({ id: '', name: '' })
+
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
     fetchPolicy: 'cache-and-network',
   })
@@ -35,21 +38,90 @@ const HasuraCRUD: VFC = () => {
     },
   })
 
-  const [] = useMutation<DeleteUserMutation>(DELETE_USER, {
+  const [delete_users_by_pk] = useMutation<DeleteUserMutation>(DELETE_USER, {
     update(cache, { data: { delete_users_by_pk } }) {
       cache.modify({
-        users(exisitingUsers, { readField }) {
-          return exisitingUsers.filter(
-            (user) => delete_users_by_pk.id !== readField('id', user)
-          )
+        fields: {
+          users(existingUsers, { readField }) {
+            return existingUsers.filter(
+              (user) => delete_users_by_pk.id !== readField('id', user)
+            )
+          },
         },
       })
     },
   })
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedUser.id) {
+      // 編集処理
+      try {
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' }) // 初期値にする
+    } else {
+      // 新規作成処理
+      try {
+        await insert_users_one({
+          variables: {
+            name: editedUser.name,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' }) // 初期値にする
+    }
+  }
+
+  if (error) {
+    return <Layout title="Hasura CRUD">Error: {error.message}</Layout>
+  }
+
   return (
-    <Layout title="Hasrura CRUD">
+    <Layout title="Hasura CRUD">
       <p className="mb-3 font-bold">Hasrura CRUD</p>
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="px-3 py-2 border border-gray-300"
+          placeholder="New user ?"
+          type="text"
+          value={editedUser.name}
+          onChange={(e) => {
+            setEditedUser({ ...editedUser, name: e.target.value })
+          }}
+        />
+        <button
+          className="disabled:opacity-40 my-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+          data-testid="new"
+          type="submit"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+        </button>
+      </form>
+      {data?.users.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            user={user}
+            setEditedUser={setEditedUser}
+            delete_users_by_pk={delete_users_by_pk}
+          />
+        )
+      })}
     </Layout>
   )
 }
+
+export default HasuraCRUD
